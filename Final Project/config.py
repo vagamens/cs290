da3781943bf33c052d394e632febca9333eee220
Kevin Clay <clay.kvn.c@gmail.com>
2017-01-12 12:24:03 -0800

    Final Project
    Added files for the final project.
    
    Added base file for Assignment 1.

---
+++ Final Project/config.py
@@ -0,0 +1,9 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__fine__))
+
+DEBUG = True
+
+ADMINS = frozenset(['youremail@yourdomain.com'])
+SECRET_KEY = 'This string will be replaced witha proper key in production.'
+
+THREADS_PER_PAGE = 8
