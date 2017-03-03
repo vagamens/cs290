9ded6ae73877e502135d533731cd44cfc2af5fcf
Kevin Clay <clay.kvn.c@gmail.com>
2017-03-02 18:08:04 -0800

    Final Project
    updated a few things in the readme
    
    added setup.py to automatically install
    Flask and Flask-wtf
    
    Changes to be committed:
    	new file:   Final Project/setup.py
    	modified:   README.md

---
+++ Final Project/setup.py
@@ -0,0 +1,48 @@
+#!/bin/env python3
+import os
+import sys
+import platform
+
+def main():
+	if(sys.version_info >= (3,0)):
+		print("Python3 detected. Continuing install.\n")
+	else:
+		print("For everything is working correctly,")
+		print("make sure that you have python3 installed.")
+		print("Please install python3")
+		return
+	if (platform.system() == 'Linux') :
+		os.system("sudo pip install flask")
+		os.system("sudo pip install flask-wtf")
+	else:
+		os.system("pip install flask")
+		os.system("sudo pip install flask-wtf")
+
+	f = False
+	wtf = False
+
+	try:
+		from flask import Flask
+		f = True
+	except:
+		f = False
+
+	try:
+		from flask.ext.wtf import Form
+		wtf = True
+	except:
+		wft = False
+
+	if (f and wtf) :
+		print("You are good to go. Just run the run.py file to run the server")
+	elif (f and not wtf) :
+		print("Flask is goo to go, but the wtforms integration is stuck.")
+		print("Just rerun this file to get it installed")
+	elif (not f):
+		print("Flask failed to install.\nYou can either rerun this file")
+		print("run the commands yourself")
+		print("On Windows:\nopen the command line and type:\npip install flask")
+		print("pip install flask-wtf")
+
+if __name__ == '__main__':
+	main()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CS290
+
+## Final Project
+
+Dependencies:
+ - Flask
+ - Flask-wtf
+
+you can either run `pip install` with Flask and Flask-wtf, or run the setup.py file,
+which will automatically install them for you.
